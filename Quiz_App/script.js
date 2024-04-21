f0ae3a21f2d6bd7692665703e00b2a1f3daf4dbc
Balaji <38598141+Balajichandra@users.noreply.github.com>
2024-04-21 17:57:57 +0530

    Add files via upload

---
+++ Quiz_App/script.js
@@ -0,0 +1,133 @@
+//const { text } = require("express");
+
+const questions = [
+    {
+        question:"Which is largest animal in the world",
+        answer: [
+            {text: "Shark",correct:false},
+            {text:"Blue whale", correct:true},
+            {text:"Elephant",correct:false},
+            {text:"Giraffe",correct:false},
+        ],
+    },
+    {
+        question:"Which is largest country in the world",
+        answer: [
+            {text: "Russia",correct:true},
+            {text:"China", correct:false},
+            {text:"USA",correct:false},
+            {text:"Canada",correct:false},
+        ],
+    },
+    {
+        question:"Who is the richest person in the world",
+        answer: [
+            {text: "Warren Buffet",correct:false},
+            {text:"Mukesh Ambani", correct:false},
+            {text:"Bernard Arnault & family",correct:true},
+            {text:"Elon Musk",correct:false},
+        ],
+    },
+    {
+        question:"What is size of int in Ubentu C ",
+        answer: [
+            {text: "2",correct:false},
+            {text:"4", correct:true},
+            {text:"8",correct:false},
+            {text:"16",correct:false},
+        ],
+    },
+    {
+        question:"What is weight of RE 350CC",
+        answer: [
+            {text: "190(KG)",correct:false},
+            {text:"170(KG)", correct:false},
+            {text:"180(KG)",correct:false},
+            {text:"185(KG)",correct:true},
+        ],
+    }
+];
+
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    // display question
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + "." + currentQuestion.question;
+
+    //display answer
+    currentQuestion.answer.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click",selectAnswer)
+    });
+}
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButton.firstChild){/*removes all previous child*/ 
+        answerButton.removeChild(answerButton.firstChild)
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButton.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again!";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click",() => {
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+})
+startQuiz();
